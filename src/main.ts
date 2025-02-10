c855d7a76a14c6d3cea3e2fb06aaeac9a4fa7610
Saxboyy <abelfalconespino@gmail.com>
2025-02-10 12:59:30 +0000

    fix: Auto Zoom input on Safari

--- src/main.ts
+++ src/main.ts
@@ -63,7 +63,7 @@ const searchPage = async (request: Request, parameters: Parameters) => {
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <title>Alfonsito Search</title>
   <link rel="stylesheet" href="main.css">
 </head>
@@ -123,7 +123,7 @@ const homePage =
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <title>Alfonsito Search</title>
   <link rel="stylesheet" href="main.css">
 </head>
